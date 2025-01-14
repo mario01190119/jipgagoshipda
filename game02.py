221daf2ebe314a3d24629cefef0116b964c167b8
mario01190119 <yoonchan2108@gmail.com>
2025-01-14 18:50:31 +0900

    Add files via upload

--- game02.py
+++ game02.py
@@ -7,38 +7,38 @@ pygame.display.set_caption("Move with keyboard")
 xp = bg.get_size()[0] // 2
 yp = bg.get_size()[1] // 2
 
-tx=0
-ty=0
+tx = 0
+ty = 0
 play = True
-while play :
-    for event in pygame.event.get() :
-        if event.type == pygame.QUIT :
+
+while play:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             play = False
-        if event.type == pygame.KEYDOWN :
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                ty = -0.3
+                ty = -1
             elif event.key == pygame.K_s:
-                ty = 0.3
+                ty = 1
             elif event.key == pygame.K_a:
-                tx = -0.3
+                tx = -1
             elif event.key == pygame.K_d:
-                tx = 0.3
-        if event.type == pygame.KEYUP :
-            if event.key == pygame.K_w :
+                tx = 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
                 ty = 0
-            elif event.key == pygame.K_s :
+            elif event.key == pygame.K_s:
                 ty = 0
-            elif event.key == pygame.K_a :
+            elif event.key == pygame.K_a:
                 tx = 0
-            elif event.key == pygame.K_d :
+            elif event.key == pygame.K_d:
                 tx = 0
 
     xp += tx
     yp += ty
-    
-    bg.fill((255, 255, 255))
-    pygame.draw.circle(bg, (0, 0, 0), (xp, yp),5)
-    pygame.display.update()
 
+    bg.fill((255, 255, 255))
+    pygame.draw.circle(bg, (0, 0, 0), (xp, yp), 5)
+    pygame.display.update()
 
 pygame.quit()
